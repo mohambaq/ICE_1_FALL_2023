aa55c8fae20446b090cdc09b58caa5abf02efe63
mohambaq <mohambaq@sheridancollege.ca>
2023-09-26 16:41:11 -0400

    ICE1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -9,24 +9,54 @@ package card;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ @Mohammed Baqar Bin Sadeq 
+ @991715469
  */
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Random baqr = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(baqr.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[baqr.nextInt(4)]);
+            magicHand[i] = c;
         }
-        
+
+        System.out.print("Enter card number: ");
+        int cardNumber = scanner.nextInt();
+        System.out.print("Enter suit: ");
+        String suit = scanner.next();
+
+        boolean isPresent = false;
+
+        for (Card card : magicHand) {
+            if (card.getValue() == cardNumber && card.getSuit().equalsIgnoreCase(suit)) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        if (isPresent) {
+            System.out.println("The card is present in the magic hand.");
+        } else {
+            System.out.println("The card is not present in the magic hand.");
+        }
+    }
+}
+
+
+    
+    
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-        // add one luckcard hard code 2,clubs
-    }
     
-}
+    
+
